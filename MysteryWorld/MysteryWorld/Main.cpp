21777d1b6514d968e0f4c55cee45a1a2cd78b54f
Whaid Cremery <whaid.cremery@outlook.fr>
2022-01-09 20:47:14 +0100

    feat: swap values

--- MysteryWorld/MysteryWorld/Main.cpp
+++ MysteryWorld/MysteryWorld/Main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+void swap(int& a, int& b);
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	int i = 2, j = 5;
+	std::cout << i << std::endl;
+	std::cout << j << std::endl;
+	swap(i, j);
+	std::cout << "swap" << std::endl;
+	std::cout << i << std::endl;
+	std::cout << j << std::endl;
 
 	return EXIT_SUCCESS;
+}
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
 }
